a561b8a911436adbdb21db6584ba69218732f70c
Jeremy Black <jb@yodacom.com>
2020-05-08 16:43:37 -0600

    added a color def and deleted comments not needed

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App({children}) {
   return (
     <ThemeProvider theme={theme}>
       <ColorModeProvider>
-        <Box w="100%" align="center" justifyItems="space-around" bg="teal.500" h="93vh">
+        <Box w="100%" align="center" justifyItems="space-around" bg="teal.500" color="black" h="93vh">
           <Stack spacing="8" align="center" justify="center">
             <CSSReset />
             {children}
@@ -30,26 +30,3 @@ function App({children}) {
 }
 
 export default App;
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
